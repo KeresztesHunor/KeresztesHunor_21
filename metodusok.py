7cf2dfcb295e5c1920ec765d742c73d5db7b8fcf
KeresztesHunor <kereszteshunor36gmail.com>
2023-01-31 10:48:37 +0100

    lapok összegzése

--- metodusok.py
+++ metodusok.py
@@ -1,11 +1,18 @@
 # megoldás
 
 
-def eredmeny(jatekos_pont: int, gep_pont: int):
-    if jatekos_pont > 21:
+def eredmeny(jatekos_lapok: [int], gep_lapok: [int]):
+    if lapok_osszege(jatekos_lapok) > 21:
         print("Játékos vesztett")
-    if gep_pont > 21:
+    if lapok_osszege(gep_lapok) > 21:
         print("Gép vesztett")
+
+
+def lapok_osszege(lapok: [int]) -> int:
+    pontok: int = 0
+    for i in range(len(lapok)):
+        pontok += lapok[i]
+    return pontok
 
 
 # teszt esetek
