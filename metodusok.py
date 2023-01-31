437940a52561ec177f9746d57782cef1e57b115c
KeresztesHunor <kereszteshunor36gmail.com>
2023-01-31 10:21:35 +0100

    paraméterek és eredmény kiírás

--- metodusok.py
+++ metodusok.py
@@ -1,8 +1,11 @@
 # megoldás
 
 
-def eredmeny():
-    pass
+def eredmeny(jatekos_pont: int, gep_pont: int):
+    if jatekos_pont > 21:
+        print("Játékos vesztett")
+    if gep_pont > 21:
+        print("Gép vesztett")
 
 
 # teszt esetek
